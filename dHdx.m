563a7c1f4cc8b0a90b1a50fed7151907d9c50100
blockahead <blockahead@gmail.com>
2017-09-24 18:54:34 +0900

    ・引数の位置を修正 ・コメントを修正

--- dHdx.m
+++ dHdx.m
@@ -1,11 +1,12 @@
 %% 函数Hの状態微分
 % x        : [ x;dx ]       （位置，速度）
+% u        : [ u;v;mu ]     （操作量，ダミー操作量，ラグランジュ乗数）
 % lmd      : [ lmd1;lmd2 ]  （随伴変数1，随伴変数2）
 % sys      : a              （システム係数）
 % sys      : b              （システム係数）
 % cgmres   : [ q1;q2 ]      （位置の重み，速度の重み）
 
-function Hx = dHdx( x, lmd, u, sys, cgmres )
+function Hx = dHdx( x, u, lmd, sys, cgmres )
     Hx = [ ...
         sys.a * lmd(2) + cgmres.q(1) * x(1); ...
         sys.b * lmd(2) * u(1) + cgmres.q(2) * x(2) + lmd(1); ...
